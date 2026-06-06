0c2054e8984e5f9ba0b6a2410d6b9fc38237f0a3
kobewi <kobewi4e@gmail.com>
2026-06-06 18:25:15 +0200

    Minimapa

---
+++ Minimap.gd
@@ -0,0 +1,18 @@
+extends Panel
+
+@export var geometry: Node
+
+func _draw() -> void:
+	draw_set_transform(Vector2(size.x * 0.5, size.y * 0.5))
+	
+	for node: CSGBox3D in geometry.get_children():
+		if node.size.y > 1:
+			var pos2d := Vector2(node.position.x, node.position.z)
+			var size2d := Vector2(node.size.x, node.size.z) * 0.5
+			
+			if size2d.x > size2d.y:
+				size2d.y = 0
+				draw_line(pos2d - size2d, pos2d + size2d, Color.GREEN)
+			else:
+				size2d.x = 0
+				draw_line(pos2d - size2d, pos2d + size2d, Color.GREEN)
