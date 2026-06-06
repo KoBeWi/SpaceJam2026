e00790494315384c2c8b9a2dbf76d061c632efb9
kobewi <kobewi4e@gmail.com>
2026-06-06 21:23:35 +0200

    Added 👻

--- Game.gd
+++ Game.gd
@@ -2,16 +2,21 @@ extends Node3D
 
 @export var level: PackedScene
 
-func _notification(what: int) -> void:
-	if what == NOTIFICATION_SCENE_INSTANTIATED:
-		var level_instance: Node3D = level.instantiate()
-		add_child(level_instance)
-		
-		$Player.position = level_instance.get_node("Start").position
-		$Player.position.y += 1
-		
-		#%Minimap.geometry.assign(level_instance.get_node("Geometry").find_children("*", "CSGBox3D"))
-		%Minimap.geometry.assign(level_instance.get_node("Geometry").get_children())
+@onready var duch: Node3D = $Ghost
+
+func _enter_tree() -> void:
+	var level_instance: Node3D = level.instantiate()
+	add_child(level_instance)
+	
+	$Player.position = level_instance.get_node("Start").position
+	$Player.position.y += 1
+	$Player.rotation.y = level_instance.get_node("Start").rotation.y
+	
+	#%Minimap.geometry.assign(level_instance.get_node("Geometry").find_children("*", "CSGBox3D"))
+	%Minimap.geometry.assign(level_instance.get_node("Geometry").get_children())
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	var dp: Vector2 = %Minimap.level.pick_random() * 2
+	duch.position = Vector3(dp.x, 0.5, dp.y)
---
+++ 👻.gd
@@ -0,0 +1,6 @@
+extends CharacterBody3D
+
+func _physics_process(delta: float) -> void:
+	if randi_range(0, 10) == 0:
+		velocity = Vector3.RIGHT.rotated(Vector3.UP, randi_range(0, 3) * (PI/2)) * 4
+	move_and_slide()
